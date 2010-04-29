ce1676cd812f910feb59d38cb306762bf4cac330
Donny Kurnia <donnykurnia@gmail.com>
2010-04-29 10:29:09 +0700

    upgrade library

--- application/libraries/MY_Controller.php
+++ application/libraries/MY_Controller.php
@@ -28,6 +28,39 @@ class MY_Controller extends Controller {
     log_message('debug', "MY_Controller Class Initialized");
   }
 
+  function _redirect_in_list($redirect_to)
+  {
+    if ( $this->is_ajax_request )
+    {
+      //form submit
+      if ( count($_POST) > 0 )
+      {
+        $this->data['reload'] = site_url($redirect_to);
+        //return json data
+        $this->output->set_output(json_encode($this->data));
+        return;
+      }
+      //not a form submit
+      redirect_js($redirect_to);
+      return;
+    }
+    //not an ajax
+    redirect($redirect_to);
+  }
+
+  function _redirect_in_form($redirect_to)
+  {
+    if ( $this->is_ajax_request )
+    {
+      $this->data['reload'] = site_url($redirect_to);
+      //return json data
+      $this->output->set_output(json_encode($this->data));
+      return;
+    }
+    //not an ajax
+    redirect($redirect_to);
+  }
+
 }
 
 /* End of file MY_Controller.php */
