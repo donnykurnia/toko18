50e1e1299798f2ca56f427f703f2edf86fc300f7
Donny Kurnia <donnykurnia@gmail.com>
2010-07-26 11:57:13 +0700

    add js files

---
+++ htdocs/assets/js/apps/penjualan_form.js
@@ -0,0 +1,20 @@
+/**
+ * @author donny
+ *
+ * required library:
+ *   jQuery
+ *   jQuery.metadata
+ *   jQuery.livequery
+ * required var:
+ *    form
+ */
+jQuery(function($){
+  //DOM loaded, code here
+  //handle datepicker
+  //date picker
+  $('#tanggal_transaksi').datepicker({
+    changeMonth: true,
+    changeYear: true,
+    dateFormat: 'yy-mm-dd'
+  });
+});
